df0a0c9e64559c2228db9a7c8fb1a4c3fba07002
Berk Akgül <berkak_gul51@hotmail.com>
2024-09-12 15:14:06 +0300

    hideImages

--- app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
+++ app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.catchthekennykotlin
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.catchthekennykotlin.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var score = 0
+    var imageArray = ArrayList<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +28,29 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        imageArray.add(binding.imageView)
+        imageArray.add(binding.imageView2)
+        imageArray.add(binding.imageView3)
+        imageArray.add(binding.imageView4)
+        imageArray.add(binding.imageView5)
+        imageArray.add(binding.imageView6)
+        imageArray.add(binding.imageView7)
+        imageArray.add(binding.imageView8)
+        imageArray.add(binding.imageView9)
+
+
         hideImages()
 
     }
 
-
+    fun hideImages(){
+        for(image in imageArray){
+            image.visibility = View.INVISIBLE
+        }
+        val random = java.util.Random()
+        val randomIndex = random.nextInt(9)
+        imageArray[randomIndex].visibility = View.VISIBLE
+    }
 
     fun increaseScore(view : View){
 
