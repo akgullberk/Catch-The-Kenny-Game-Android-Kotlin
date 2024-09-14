94081976fe43c619e411660d26e5ab7300f3d2ef
Berk Akgül <berkak_gul51@hotmail.com>
2024-09-14 13:10:47 +0300

    runnable

--- app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
+++ app/src/main/java/com/example/catchthekennykotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.catchthekennykotlin
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var score = 0
     var imageArray = ArrayList<ImageView>()
+    var runnable = Runnable {}
+    var handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,12 +48,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hideImages(){
-        for(image in imageArray){
-            image.visibility = View.INVISIBLE
+
+        runnable = object : Runnable{
+            override fun run() {
+                for(image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
+                val random = java.util.Random()
+                val randomIndex = random.nextInt(9)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+
         }
-        val random = java.util.Random()
-        val randomIndex = random.nextInt(9)
-        imageArray[randomIndex].visibility = View.VISIBLE
+        handler.post(runnable)
     }
 
     fun increaseScore(view : View){
